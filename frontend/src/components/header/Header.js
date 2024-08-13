06b39253c9a7297dea65a2af0671c53b57165bf7
Rishav Barman <rishavbarman.dev@gmail.com>
2024-08-13 20:23:01 +0530

    updated the header component and make it responsive

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,52 +1,43 @@
 import React, { useState } from "react";
 import "./Header.css";
+import cart from "../../assets/cart.png";
+import user from "../../assets/user.png";
 
 const Header = () => {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  const handleNavToggle = () => {
+    setIsNavVisible(!isNavVisible);
+  };
+
   return (
     <>
       <header className="header">
-        <div className="row container">
-          <h1 className="header-title">GadgetGrove</h1>
-          <nav className="nav">
+        <div className="container row">
+          <div className="nav-container">
+            <button
+              onClick={handleNavToggle}
+              className="nav-toggle"
+              aria-label="open-navigation"
+            >
+              <span className="hamburger"></span>
+            </button>
+            <a className="header-link" href="/">
+              <h1 className="header-title">GadgetGrove</h1>
+            </a>
+          </div>
+
+          <nav className={`nav ${isNavVisible ? "nav--visible" : ""}`}>
             <ul className="nav-list">
               <li className="nav-items">
                 <a href="/cart" className="nav-link">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="#6e7278"
-                    stroke-width="1.75"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    className="lucide lucide-shopping-cart"
-                  >
-                    <circle cx="8" cy="21" r="1" />
-                    <circle cx="19" cy="21" r="1" />
-                    <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
-                  </svg>
+                  <img src={cart} className="shopping-cart" alt="cart-logo" />
                   Cart
                 </a>
               </li>
               <li className="nav-items">
                 <a href="/signin" className="nav-link">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="#6e7278"
-                    stroke-width="1.75"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    className="lucide lucide-user"
-                  >
-                    <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                    <circle cx="12" cy="7" r="4" />
-                  </svg>
+                  <img src={user} className="shopping-cart" alt="user-logo" />
                   Sign In
                 </a>
               </li>
