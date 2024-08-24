42342f590274dbafa24a73ec0a2e1fbf05fdcc9f
Rishav Barman <rishavbarman.dev@gmail.com>
2024-08-24 12:46:35 +0530

    add alt property in Product.js

--- frontend/src/components/product/Product.js
+++ frontend/src/components/product/Product.js
@@ -15,10 +15,11 @@ const Product = () => {
                   key={product._id}
                   className="product-img"
                   src={product.image}
+                  alt="products images"
                 />
                 <h3 className="product-name">{product.name}</h3>
                 <h3 className="product-rating">{product.rating}</h3>
-                <h3 className="product-price">{product.price}</h3>
+                <h3 className="product-price">Rs. {product.price} </h3>
               </div>
             </div>
           ))}
