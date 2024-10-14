ec6ed13e88ce7ac31323518a694c0d935802b0ca
Rishav Barman <rishavbarman.dev@gmail.com>
2024-10-14 16:20:13 +0530

    bringing redux state into HomeScreen

--- frontend/src/screens/HomeScreen/HomeScreen.js
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -1,27 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Product from "../../components/product/Product";
 import "./HomeScreen.css";
-import axios from "axios";
+import { listProducts } from "../../actions/productActions";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  console.log(products);
 
   return (
     <div className="container">
       <h1 className="product-title">Latest Products</h1>
-      <div className="product-row">
-        {products.map((product) => (
-          <Product product={product} />
-        ))}
-      </div>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <div className="product-row">
+          {products?.map((product) => (
+            <Product product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
