e97aa2fe6cd53ab6ca8f1c9a7a6c67534993d62a
Rishav Barman <96981398+rishavbarman-dev@users.noreply.github.com>
2026-01-22 21:21:41 +0530

    Log API URL from environment variables
    Add console log for API URL

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -24,6 +24,7 @@ import {
 } from "../constants/productConstants";
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+console.log(process.env.REACT_APP_API_URL)
 
 export const listProducts =
   (keyword = "", pageNumber = "") =>
